cae42407bcede63ed07a86d3dbe540161524df71
wsg <1305676305@qq.com>
2021-12-17 21:34:14 +0800

    新增部分页面

---
+++ app/src/main/java/com/wsg/lover/activity/LoveActivity.kt
@@ -0,0 +1,19 @@
+package com.wsg.lover.activity
+
+import android.os.Bundle
+import com.wsg.lover.R
+import com.wsg.lover.base.BaseActivity
+
+/**
+ * Create on 2021/11/20.
+ *
+ * @author wangshengguo.
+ */
+class LoveActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_love)
+    }
+
+}
---
+++ app/src/main/java/com/wsg/lover/activity/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.wsg.lover.activity
+
+import android.os.Bundle
+import com.wsg.lover.R
+import com.wsg.lover.base.BaseActivity
+
+/**
+ * Create on 2021/11/20.
+ *
+ * @author wangshengguo.
+ */
+class MainActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
